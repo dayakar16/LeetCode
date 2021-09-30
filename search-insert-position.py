c50c8d7b98d07a1878111c8bde936255848a5e2a
Dayakar Ravuri <dayakar16@gmail.com>
2021-09-30 17:44:07 -0400

    Binary search algorithms

---
+++ search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0 
+        right = len(nums) - 1
+        mid = 0
+        while left <= right: 
+            mid = (right + left)// 2
+            if nums[mid] == target: 
+                return mid
+            elif nums[mid] < target: 
+                left = mid + 1 
+            else: 
+                right = mid - 1 
+        return left
+        
